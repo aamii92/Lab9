4183e819f3ccf562282aa56b7f6edab51dd8963f
Shagai-123 <mungunshagaishiirev@gmail.com>
2025-11-13 19:22:02 +0800

    Add divisions and job descriptions in main function

--- main.cpp
+++ main.cpp
@@ -3,5 +3,10 @@
 #include<string>
 using namespace std;
 int main(){
-    
+    Division techDivision("techDivision");
+    Division hrDivision("hrDivisio");
+    Division financeDivision("financeDivision");
+    auto pmJob = std::make_shared<JobDescription>("programmer");
+    auto mgJob = std::make_shared<JobDescription>("manager");
+    auto anJob = std::make_shared<JobDescription>("analyst");}
 }
